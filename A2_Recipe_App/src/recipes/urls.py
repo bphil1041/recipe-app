71477e96fdf2c6b5c93a70e743b05b37f9869068
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-09 08:27:24 -0500

    Update urls.py
    add about_view

--- A2_Recipe_App/src/recipes/urls.py
+++ A2_Recipe_App/src/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, RecipeListView, recipe_detail_view, login_view, logout_view
+from .views import home_view, RecipeListView, recipe_detail_view, login_view, logout_view, about_view
 
 urlpatterns = [
     path('', home_view, name='home'),  # Homepage
@@ -9,4 +9,5 @@ urlpatterns = [
          name='recipes_list'),  # Recipes list view
     path('recipes/<int:recipe_id>/', recipe_detail_view,
          name='recipe_detail'),  # Recipe detail URL
+    path('about/', about_view, name='about'),  # About Me page
 ]
