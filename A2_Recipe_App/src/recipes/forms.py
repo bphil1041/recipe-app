3e7e91d973581e1b33a5ca103259aeac29e76d94
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-10 23:17:54 -0500

    Update forms.py

--- A2_Recipe_App/src/recipes/forms.py
+++ A2_Recipe_App/src/recipes/forms.py
@@ -1,5 +1,7 @@
 # src/recipes/forms.py
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 from .models import Recipe
 
@@ -9,3 +11,18 @@ class RecipeForm(forms.ModelForm):
         model = Recipe
         fields = ['title', 'description',
                   'ingredients', 'cooking_time', 'difficulty']
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title', 'description', 'ingredients',
+                  'cooking_time', 'difficulty', 'image']
