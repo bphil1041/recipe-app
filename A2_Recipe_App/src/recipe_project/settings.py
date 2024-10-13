c8ecacb74176483cbb3c0d1110c8eaba22090b2b
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-13 00:59:29 -0500

    Update settings.py

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -15,7 +15,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lit-ridge-08816-59889bf26ea5.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "lit-ridge-08816-59889bf26ea5.herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 INSTALLED_APPS = [
